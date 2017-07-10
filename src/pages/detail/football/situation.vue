6c27a23c74147a8b6b609d918e59a8fa3bd82f39
sampsonli <740056710@qq.com>
2017-07-10 17:47:00 +0800

    modify

--- src/pages/detail/football/situation.vue
+++ src/pages/detail/football/situation.vue
@@ -1,18 +1,30 @@
 <template>
     <div>
+       <!-- <template v-if="(situation.eventlist && situation.eventlist.length) || (situation.statistic && situation.statistic.h_ballcontrol_rate)">
+
+        </template>-->
         <template v-if="!feature.a[match.status]">
             <event v-if="situation.eventlist && situation.eventlist.length" :eventlist="situation.eventlist" :status="match.status"></event>
             <statistic v-if="situation.statistic && situation.statistic.h_ballcontrol_rate" :statistic="situation.statistic"></statistic>
             <me-sports v-if="situation.news && situation.news.length" :news="situation.news" :init-size="3"></me-sports>
             <div class="sk-btips"
-                 v-if="(situation.eventlist && situation.eventlist.length) || (situation.statistic && situation.statistic.h_ballcontrol_rate != null)">
+                 v-if="(situation.eventlist && situation.eventlist.length) || (situation.statistic && situation.statistic.h_ballcontrol_rate)">
                 500彩票网提示：<br>以上数据仅供参考，请以官方公布的数据为准
             </div>
+
+
+
         </template>
         <template v-else>
             <me-sports v-if="situation.news && situation.news.length" :news="situation.news"  :init-size="match.status == StatusCode.NOT_STARTED?5:3"></me-sports>
 
         </template>
+
+        <div class="ui-empty" v-if="noData">
+            <img src="http://tccache.500.com/mobile/widget/empty/images/07.png" class="w240">
+            <div class="ui-empty-dfont">比赛时间 {{match.matchtime.substr(5, 11)}}</div>
+            <div class="ui-empty-gfont">先去分析栏目看看吧</div>
+        </div>
 
     </div>
 </template>
@@ -79,6 +91,17 @@
             },
             loaded () {
                 return this.$store.state.refreshing === 0
+            },
+            noData () {
+                if (!this.loaded) {
+                    return false
+                }
+                if (!this.feature.a[this.match.status]) {
+                    return !((this.situation.statistic && this.situation.statistic.h_ballcontrol_rate) ||
+                    (this.situation.eventlist && this.situation.eventlist.length) || (this.situation.news && this.situation.news.length))
+                } else {
+                    return !(this.situation.news && this.situation.news.length)
+                }
             }
 
         },
@@ -87,3 +110,13 @@
         }
     }
 </script>
+<style>
+    /*.ui-empty{position:absolute;top:50%;left:50%;-webkit-transform:translate(-50%,-50%);transform:translate(-50%,-50%);text-align:center;display:inline-block;}*/
+    .ui-empty{padding:2.72rem 0;text-align:center;}
+    .ui-empty img{margin-bottom:0.933333rem;}
+    .ui-empty .w240{width:3.2rem;}
+
+    .ui-empty-dfont{font-size:0.453333rem;color:#333;height:0.893333rem;line-height:0.893333rem;}
+    .ui-empty-gfont{font-size:0.4rem;color:#b3b3b3;margin-bottom:0.773333rem;padding:0 0.5rem;}
+
+</style>
