84886a89de90d1ea36ca283b0b97f2488e20383e
子臻 <814589327@qq.com>
2017-07-21 17:33:23 +0800

    样式修改 足球头部

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -4,6 +4,7 @@
     </div>
 </template>
 <script>
+    import '~assets/style/public/reset.css'
     import Vue from 'vue'
     import vueTap from 'v-tap'
     Vue.use(vueTap)
@@ -13,7 +14,9 @@
         }
     }
 </script>
-<style>
+
+
+<styleq>
     .l-box-center{display:-webkit-box; -webkit-box-align: center; -webkit-box-pack: center;display:flex;justify-content: center;align-items: center;  -webkit-box-orient: vertical;flex-flow: column;}
     .l-box-vertical-center{display: -webkit-box;-webkit-box-align: center;display: flex;align-items: center;-webkit-box-orient: horizontal; flex-flow: row;}
     .l-box-vertical-center-justify{display: -webkit-box;-webkit-box-orient: horizontal; flex-flow: row; -webkit-box-align: center;-webkit-box-pack: justify;display: flex;align-items: center;justify-content: space-between;}
@@ -36,4 +39,86 @@
         bottom: 0;
         overflow: hidden;
     }
-</style>
+    body {
+        -webkit-user-select: none;
+        user-select: none;
+        outline: 0;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        -moz-tap-highlight-color: rgba(0, 0, 0, 0);
+        -ms-tap-highlight-color: rgba(0, 0, 0, 0);
+        tap-highlight-color: rgba(0, 0, 0, 0)
+    }
+    html, body, dd, dl, dt, fieldset, form, h1, h2, h3, h4, h5, h6, hr, input, label, li, ol, p, ul {
+        margin: 0;
+        padding: 0;
+        font-weight: 400
+    }
+    body {
+        font-size: 15px;
+        /*no*/
+        -webkit-text-size-adjust: none;
+        font-family: -apple-system, "Helvetica Neue", Arial, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", SimSun, sans-serif;
+        background: #efefef;
+        width: 100%;
+        color: #242c34
+    }
+    a {
+        text-decoration: none
+    }
+    em, i, strong, s {
+        font-style: normal
+    }
+    li, ul {
+        list-style: none
+    }
+    table {
+        border-collapse: collapse
+    }
+    td {
+        empty-cells: show;
+        color: #242c35;
+    }
+    button {
+        border: 0
+    }
+    cite, i {
+        font-style: normal
+    }
+    input[type=text], input[type=number], input[type=password] {
+        -webkit-appearance: none
+    }
+    input, textarea {
+        outline: 0;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    }
+    .fl {
+        float: left
+    }
+    .fr {
+        float: right
+    }
+    .clear {
+        zoom: 1
+    }
+    .textleft {
+        text-align: left
+    }
+    .textright {
+        text-align: right
+    }
+    .textcenter {
+        text-align: center
+    }
+    .clear:after {
+        content: '';
+        clear: both;
+        display: block;
+        height: 0;
+        visibility: hidden
+    }
+    .f20{font-size:0.266rem}
+    .f24{font-size:0.32rem}
+    .f26{font-size:0.346rem}
+    .f28{font-size:0.373rem}
+    .f30{font-size:0.4rem}
+</styleq>
