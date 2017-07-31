d2d8dc8ecd8d24e29c764355727b4f6da6bd1495
zhangzc <mrzzhichao@gmail.com>
2017-07-31 16:23:31 +0800

    finish hotcool

--- src/pages/bfyc/hotcool.vue
+++ src/pages/bfyc/hotcool.vue
@@ -1,6 +1,78 @@
 <!--ui: http://ui.touch500.boss.com/touch/bifen/v124/record.html-->
 <template>
+    <div class="l-full l-flex-column">
+        <!--顶部的时间-->
+        <section class="pre-header">
+            <ul>
+                <li>比赛</li>
+                <li v-for="(name, type) in dataType">{{name}}</li>
+            </ul>
+        </section>
 
+
+        <div class="hotc-box l-flex-1 l-relative" v-if="hotcool">
+            <div v-if="hotcool.matches.length" class="l-full l-scroll-y">
+                <!--冷热列表-->
+                <section class="hotc-cont" v-for="(match, idx) in hotcool.matches" v-if="match.status !== '4'"
+                         v-tap="{methods: goAnalysis, fid: match.fid}">
+                    <div class="hotc-tab-tit ">{{match | makeTitle}}<em :class="descClass[idx]">{{match.tag.desc}}</em>
+                    </div>
+                    <ul class="hotc-tab-list">
+                        <li class="hotc-tab-item">
+                            <ul>
+                                <li>{{match.homesxname}}<em v-if="match.result==='3'">赢</em></li>
+                                <li v-for="(name, type) in dataType">{{match.cell.win[type] | dataFmt(type)}}</li>
+                            </ul>
+                        </li>
+                        <li class="hotc-tab-item">
+                            <ul>
+                                <li>平局<em v-if="match.result==='1'">赢</em></li>
+                                <li v-for="(name, type) in dataType">{{match.cell.draw[type] | dataFmt(type)}}</li>
+                            </ul>
+                        </li>
+                        <li class="hotc-tab-item">
+                            <ul>
+                                <li>{{match.awaysxname}}<em v-if="match.result==='0'">赢</em></li>
+                                <li v-for="(name, type) in dataType">{{match.cell.lost[type] | dataFmt(type)}}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
+
+                <!--已完场-->
+                <div class="tag-game-over" v-if="curStatus.history&&curStatus.latest">已完场</div>
+                <section class="hotc-cont" v-for="(match, idx) in hotcool.matches" v-if="match.status === '4'"
+                         v-tap="{methods: goAnalysis, fid: match.fid}">
+                    <div class="hotc-tab-tit ">{{match | makeTitle}}<em :class="descClass[idx]">{{match.tag.desc}}</em>
+                    </div>
+                    <ul class="hotc-tab-list">
+                        <li class="hotc-tab-item">
+                            <ul>
+                                <li>{{match.homesxname}}<em v-if="match.result==='3'">赢</em></li>
+                                <li v-for="(name, type) in dataType">{{match.cell.win[type] | dataFmt(type)}}</li>
+                            </ul>
+                        </li>
+                        <li class="hotc-tab-item">
+                            <ul>
+                                <li>平局<em v-if="match.result==='1'">赢</em></li>
+                                <li v-for="(name, type) in dataType">{{match.cell.draw[type] | dataFmt(type)}}</li>
+                            </ul>
+                        </li>
+                        <li class="hotc-tab-item">
+                            <ul>
+                                <li>{{match.awaysxname}}<em v-if="match.result==='0'">赢</em></li>
+                                <li v-for="(name, type) in dataType">{{match.cell.lost[type] | dataFmt(type)}}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
+
+            </div>
+
+            <prompt v-else type="no-data" tip0="暂无数据"/>
+        </div>
+        <prompt v-else type="loading" tip0="正在加载中..."/>
+    </div>
 
 
 </template>
@@ -93,6 +165,11 @@
             },
             makeTitle(item) {
                 return item.order + ' ' + item.simpleleague + ' ' + item.matchtime.slice(5)
+            },
+            dataFmt(input, type) {
+                let tail = ''
+                if(type === 'europe' || type === 'betfair') tail = '%'
+                return input === '' ? '--' : (input + tail)
             }
         }
 
