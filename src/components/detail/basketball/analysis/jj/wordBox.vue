c15b62d29cd14fd11b4123dff778ba68f5e07490
sampsonli <740056710@qq.com>
2017-08-01 12:04:30 +0800

    删除没用的html

--- src/components/detail/basketball/analysis/jj/wordBox.vue
+++ src/components/detail/basketball/analysis/jj/wordBox.vue
@@ -1,33 +1,29 @@
 <template>
-    <div class="popBox slide-bottom-to-top drunk-created" style="animation-fill-mode:both">
-    	<div class="popLayer">
-    	</div>
-    	<div class="l-full l-flex-column">
-    		<div class="popTopbar" v-tap="{methods: closeDialog}">
-    		</div>
-    		<div class="popInner box-yc">
-    			<div class="full-scroll">
-    				<div class="mcjs-cont">
-    					<ul class="mcjs-list">
-    						<li :class="{'cur': tab === type}" v-for="(item, type) in params.wordData" v-tap="{methods: () => tab = type}">{{item.name}}</li>
-    					</ul>
-    				</div>
-    				<div class="mcjs-detail">
-    					<div class="mcjs-tit">
-    						{{params.wordData[tab].tit}}
-    					</div>
-    					<p>
-                            {{params.wordData[tab].desc}}
-    					</p>
-    				</div>
-    			</div>
-    		</div>
-    		<div class="popFooter" v-tap="{methods: closeDialog}">
-    			<span class="arrow"></span>
-    			<h2 class="f30">{{params.title}}</h2>
-    		</div>
-    	</div>
-    </div>
+	<div class="l-full l-flex-column">
+		<div class="popTopbar" v-tap="{methods: closeDialog}">
+		</div>
+		<div class="popInner box-yc">
+			<div class="full-scroll">
+				<div class="mcjs-cont">
+					<ul class="mcjs-list">
+						<li :class="{'cur': tab === type}" v-for="(item, type) in params.wordData" v-tap="{methods: () => tab = type}">{{item.name}}</li>
+					</ul>
+				</div>
+				<div class="mcjs-detail">
+					<div class="mcjs-tit">
+						{{params.wordData[tab].tit}}
+					</div>
+					<p>
+						{{params.wordData[tab].desc}}
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="popFooter" v-tap="{methods: closeDialog}">
+			<span class="arrow"></span>
+			<h2 class="f30">{{params.title}}</h2>
+		</div>
+	</div>
 </template>
 
 <script>
