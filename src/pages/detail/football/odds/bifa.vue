412f9ad4a5c385340a5b2000faa69f3d486a0c22
sampsonli <740056710@qq.com>
2017-08-04 17:47:35 +0800

    fixbug

--- src/pages/detail/football/odds/bifa.vue
+++ src/pages/detail/football/odds/bifa.vue
@@ -507,7 +507,13 @@
         background: #437ba8
     }
     /*over*/
-
+    .feed-back{width:100%;height:1.733333rem;background:#fff;border-top:1px solid #eaeaea}
+    .feed-box{color:#787878;line-height:1.733333rem;text-align:center}
+    [data-dpr="1"] .feed-box{font-size:13px}
+    [data-dpr="2"] .feed-box{font-size:26px}
+    [data-dpr="3"] .feed-box{font-size:39px}
+    .feed-box i{width:.4666rem;height:.4533rem;display:inline-block;background:url(/mobile/touch/images/bifen/icon_sprite.png) no-repeat;background-size:1.92rem;margin-right:.1333rem;position:relative;top:.1rem}
+    .feed-box:active{background:#eee}
 
 
 </style>
