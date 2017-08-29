5863f3720cd756a169f0ee43d2d4b4e4a7fc8a09
sampsonli <740056710@qq.com>
2017-08-29 15:27:25 +0800

    fixbug

--- server.js
+++ server.js
@@ -81,9 +81,9 @@ app.use((req, resp, next) => {
             req,
             resp,
             host: 'ews.500.com',
-            ip: '43.247.69.20',
+            // ip: '43.247.69.20',
             // ip: 'ews.500.com',
-            // ip: '10.0.1.31',
+            ip: '10.0.1.31',
             path: req.originalUrl.replace('/ews', '')
         })
     } else {
