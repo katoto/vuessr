17135b036dffb806e1bd4c9f99ba79cebd6357a3
子臻 <814589327@qq.com>
2017-09-04 17:51:39 +0800

    123

--- src/components/home/zqListItem.vue
+++ src/components/home/zqListItem.vue
@@ -321,6 +321,9 @@
         line-height: .64rem;
         overflow: hidden
     }
+    .five-score .first-score{
+        width:0.54rem;
+    }
 
     .score-half .second-half {
         font-size: .453333rem;
@@ -344,7 +347,7 @@
     .game-name em {
         font-size: .293333rem;
         color: #aab5bd;
-        margin-left: .213333rem;
+        margin-left: .13333rem;
     }
     .game-name .red-c {
         display: inline-block;
