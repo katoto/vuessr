ff56b26d399e2f03888b9adebd3cb9988de8653d
guxiaojuan <1634747911@qq.com>
2017-09-06 12:00:02 +0800

    ''

--- src/pages/detail/football/analysis/zj.vue
+++ src/pages/detail/football/analysis/zj.vue
@@ -1248,5 +1248,9 @@
     .zd-arrow{width:.533333rem;height:.177rem;position:absolute;top:50%;left:50%;margin:-.2rem 0 0 -.266667rem;background-position:center -10.84rem}
     .rotate180{-webkit-animation:all .2s linear;animation:all .2s linear;-webkit-transform:rotate(180deg);transform:rotate(180deg)}
 
-
+    tr.vs-ing td {
+        background: #f6fafe;
+        border-bottom: 1px solid #f2f8fd;
+        border-top: 1px solid #f2f8fd;
+    }
 </style>
