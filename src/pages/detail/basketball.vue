9489e899dd8f71aa0b69c30f39724a52546813f8
guxiaojuan <1634747911@qq.com>
2017-09-18 10:09:46 +0800

    '头部隐藏'

--- src/pages/detail/basketball.vue
+++ src/pages/detail/basketball.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="l-full l-flex-column root" v-if="baseInfo">
         <div class="detailTop" style="display: block;" :class="{'topBarMove': showScore, 'topBarMove2': !showScore}">
-            <a class="back-icon" onclick="history.back()" href="javascript:;">返回</a>
-            <router-link2  :to="{path: '/home/zq/jclq/cur', query: $route.query}" class="link-index f26">比分首页</router-link2>
+            <a class="back-icon" onclick="history.back()" href="javascript:;" v-if="$route.query.from!=='app_bet'">返回</a>
+            <router-link2  :to="{path: '/home/zq/jclq/cur', query: $route.query}" class="link-index f26" v-if="$route.query.from!=='app_bet'">比分首页</router-link2>
             <div v-tap="{methods:goLeague,seasonid:baseInfo.seasonid}" class="r-sn f24">{{baseInfo.simpleleague}}</div>
 
 
