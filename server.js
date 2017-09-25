9f522e6c46bed677e9b221ad895bb3130986d3a8
sampsonli <740056710@qq.com>
2017-09-25 11:59:06 +0800

    optimate

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ function render (req, res) {
     const s = Date.now()
 
     res.setHeader('Content-Type', 'text/html')
-    res.setHeader('Cache-Control', 'max-age=120') // 缓存2分钟
+    // res.setHeader('Cache-Control', 'max-age=120') // 缓存2分钟
     /* const handleError = err => {
         if (err && err.code === 404) {
             res.status(404).end('404 | Page Not Found')
