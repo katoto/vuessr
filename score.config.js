b04ebb1f6ada8c9b9350767ca8304d041c3dabd4
sampsonli <740056710@qq.com>
2017-09-25 12:20:36 +0800

    去掉热更新

--- score.config.js
+++ score.config.js
@@ -9,11 +9,11 @@ module.exports = {
             kill_timeout: 3000,
             instances: 3,
             exec_mode: 'cluster',
-            watch: ['dist/*.json'],
+            // watch: ['dist/*.json'],
             restart_delay: '500', // time to wait before restarting a crashed app (in milliseconds). defaults to 0.,
             listen_timeout: '5000', // time in ms before forcing a reload if app not listening,
             max_memory_restart: '1280M',
-            ignore_watch: ['node_modules'],
+            // ignore_watch: ['node_modules'],
             env: {
                 NODE_ENV: 'production',
                 PORT: 3000,
